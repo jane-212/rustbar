0892808a933a342a4482de9f3ec0b30dd69a7734
jane <jjt1525@outlook.com>
2022-11-23 17:20:05 +0800

    add interval to version update

--- src/version.rs
+++ src/version.rs
@@ -2,11 +2,17 @@ use crate::bar::Module;
 use crate::error;
 use async_trait::async_trait;
 use std::path::PathBuf;
-use tokio::fs;
+use tokio::{
+    fs,
+    time::{Duration, Instant},
+};
 
 pub struct Version {
     filename: PathBuf,
     version: String,
+    last_tick: Instant,
+    interval: Duration,
+    is_fisrt: bool
 }
 
 impl Default for Version {
@@ -14,6 +20,9 @@ impl Default for Version {
         Self {
             filename: PathBuf::from("/proc/version"),
             version: String::new(),
+            last_tick: Instant::now(),
+            interval: Duration::from_secs(60 * 60 * 12),
+            is_fisrt: true
         }
     }
 }
@@ -21,12 +30,26 @@ impl Default for Version {
 #[async_trait]
 impl Module for Version {
     async fn update(&mut self) -> error::IResult<()> {
-        self.version = fs::read_to_string(&self.filename)
-            .await?
-            .split_whitespace()
-            .nth(2)
-            .unwrap_or("unknown")
-            .to_owned();
+        if self.is_fisrt {
+            self.version = fs::read_to_string(&self.filename)
+                .await?
+                .split_whitespace()
+                .nth(2)
+                .unwrap_or("unknown")
+                .to_owned();
+            self.is_fisrt = false;
+            self.last_tick = Instant::now();
+        }
+
+        if self.last_tick.elapsed() >= self.interval {
+            self.version = fs::read_to_string(&self.filename)
+                .await?
+                .split_whitespace()
+                .nth(2)
+                .unwrap_or("unknown")
+                .to_owned();
+            self.last_tick = Instant::now();
+        }
 
         Ok(())
     }
